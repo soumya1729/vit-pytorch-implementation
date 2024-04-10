5a96ee50f249b3db797abcc5867f3131bd5a2c40
SOUMYADIP MAL <32342488+soumya1729@users.noreply.github.com>
2024-04-11 03:34:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'vit-pytorch-implementation',
-  version = '1.0.0',
+  version = 'v1.0.0-beta',
   license='MIT',
   description = 'Vision Transformer (ViT) - Pytorch',
   long_description_content_type = 'text/markdown',
